26eefeabdb435e4184414fb9832507a795ba5e50
YFisch <yfisch381@gmail.com>
2023-07-31 13:45:06 -0400

    Update and Insert Sproc

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -12,7 +12,7 @@ namespace CPUWindowsFormFramework
             lst.DataBindings.Add("SelectedValue", targetdt, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
         }
 
-        public static void SetControlBinding(Control ctrl, DataTable dt)
+        public static void SetControlBinding(Control ctrl, BindingSource bindsource)
         {
             string propertyname = "";
             string controlname = ctrl.Name.ToLower();
@@ -33,7 +33,7 @@ namespace CPUWindowsFormFramework
 
             if (propertyname != "" && columnname != "")
             {
-                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+                ctrl.DataBindings.Add(propertyname, bindsource, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
             }
 
         }
